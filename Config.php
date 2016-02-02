d2492250e242042fc027c23cb6eb386bb0f48ede
skambo <swairimu2002@gmail.com>
2016-02-02 19:14:06 +0300

    Update Config.php

--- Config.php
+++ Config.php
@@ -3,7 +3,7 @@
 class Config {
     
     const URL = "https://raw.githubusercontent.com/onaio/ona-tech/master/data/water_points.json";
-    const IS_FUNCTIONAL="yes";
+    const Is_Functional="yes";
     const Incorrect_Response ="Incorrect_Response";
     
 }

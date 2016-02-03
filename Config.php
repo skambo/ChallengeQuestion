79961c7f5ad21d1cb8abf1e4ff4b67e94ddd17f8
skambo <swairimu2002@gmail.com>
2016-02-03 10:51:03 +0300

    Delete Config.php

--- Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Config {
-    
-    const URL = "https://raw.githubusercontent.com/onaio/ona-tech/master/data/water_points.json";
-    const Is_Functional="yes";
-    const Incorrect_Response ="Incorrect_Response";
-    
-}

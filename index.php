070e029f39c685a7600d429938c92a51bde0cf34
skambo <swairimu2002@gmail.com>
2016-02-03 10:48:57 +0300

    Update index.php
    Answers to challenge questions

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 $url = "https://raw.githubusercontent.com/onaio/ona-tech/master/data/water_points.json";
-
+//Define the header request
+define("USAGE", "CHALLENGE");
+//Rank percentages
+define("RATE", 100);
+//Format final value
+define("FORMAT", "%.2f");
 //  Initiate curl
 $ch = curl_init();
 // Disable SSL verification
@@ -16,24 +21,119 @@ $result=curl_exec($ch);
 curl_close($ch);
 
 // Will dump a beauty json :3
-$result = json_decode($result, true);
-echo'<table>';
-foreach ($result as $results){
+$dataset = json_decode($result, true);
+$calc = Calculate($dataset);
 
-        echo'<tr>'; 
-        echo'<td>'. $results['water_pay']."</td>";
-        echo'<td>'. $results['respondent'].'</td>';
-        echo'<td>'. $results['research_asst_name'].'</td>';
-		 echo'<td>'. $results['water_used_season']."</td>";
-        echo'<td>'. $results['_bamboo_dataset_id'].'</td>';
-        echo'<td>'. $results['_deleted_at'].'</td>';
-		 echo'<td>'. $results['water_point_condition']."</td>";
-        echo'<td>'. $results['_xform_id_string'].'</td>';
-        echo'<td>'. $results['other_point_1km'].'</td>';
-        echo'<tr>';
+    //Display the response in table format
+	print "<strong>Functional Water Points</strong>: ".$calc['functional_water_points']."<br>";
+	
+	echo  "<table><th>Number of Water Points<br></th>";
+	foreach ($calc['number_of_water_points'] as $waterpts=>$item){
+    echo "<tr><td>".$waterpts."</td><td>".$item."</td></tr>";
+}
+echo "</table>";
+    echo "<table><th>Community Name Rating</th>";
+	foreach ($calc['community_ranking'] as $ranking=>$item){
+    echo "<tr><td>".$ranking."</td><td>".$item."</td></tr>";
+}
+echo "</table>";
+
+function Calculate($dataset) {
+        $counts = array();
+        $community_array = array(); //the number of water points in the community
+        $community_arrayx = array(); //the non-functional water points
+        $ranking_array = array(); //rank by percentage
+        $counter = 0;
+        
+        foreach ($dataset as $data => $value) {
+            if (isset($value['water_functioning'])) {
+                $stat = $value['water_functioning'];
+                $community = $value['communities_villages'];
+                if (!in_array($community, $community_arrayx)) {
+                    array_push($community_arrayx, $community);
+                }                
+                if (strcasecmp($stat, "yes") == 0) {
+                    ++$counter;
+                }
+            }
+        }
+
+        $cc_arr=array();
+        
+        for ($y = 0; $y < count($community_arrayx); ++$y) {
+            $comm = $community_arrayx[$y];
+            $community_array[$comm] 
+                    = getWaterPointsToTal($dataset, $comm);
+            
+            $cc_arr[$comm] 
+                    = getBrokenWaterPointsToTal($dataset, $comm);
+            $ranking_array = $cc_arr;
+        }
+        
+                
+        $counts['functional_water_points'] = $counter;
+        $counts['number_of_water_points'] = $community_array;
+        $counts['community_ranking'] = getRanking($ranking_array);
+
+        return $counts;
     }
-echo'</table>';
-
+	
+	function getWaterPointsToTal($dataset, $community,
+            $functioning = 'yes') {
+        $counter = 0;
+        foreach ($dataset as $data => $value) {
+            if (isset($value['communities_villages'])) {
+                $community_cmp = $value['communities_villages'];
+                if (strcasecmp($community, $community_cmp) == 0) {
+                    if ($functioning == NULL || empty($functioning)) {
+                        ++$counter;
+                    } else {
+                        $stat = $value['water_functioning'];
+                        if (strcasecmp($functioning, $stat) == 0) {
+                            ++$counter;
+                        }
+                    }
+                }
+            }
+        }
+        return $counter;
+    }
+	
+	function getBrokenWaterPointsToTal($dataset, $community) {
+        $counter = 0;
+        foreach ($dataset as $data => $value) {
+            if (isset($value['communities_villages'])) {
+                $community_cmp = $value['communities_villages'];
+                if (strcasecmp($community, $community_cmp) == 0) {
+                    if(isset($value['water_point_condition'])){
+                        $stat = $value['water_point_condition'];
+                        if (strcasecmp($stat, "broken") == 0) {
+                            ++$counter;
+                        }
+                    }
+                }
+            }
+        }
+        return $counter;
+    }
+	
+	function getRanking($data) {
+        $values = array();
+        $total=0;
+        foreach ($data as $community=>$broken_water_points){
+            $total=$total+$broken_water_points;
+        }
+        //Calculate the percentages
+        $perc = array();
+        foreach ($data as $community=>$broken_water_points){
+            $p = ($broken_water_points*RATE)/$total;
+            $perc[$community]=  sprintf(FORMAT, $p);
+        }
+        //Sort array based on percentages
+        array_multisort($perc);
+        //Return the final values.
+        return $perc;
+    }
 ?>
 Attachments area
 Preview attachment Software Engineer Challenge Question
